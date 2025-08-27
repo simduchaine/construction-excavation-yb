f2047d07a8cc6b3a5f7e5324ad08817c4ece9db5
Simon Duchaine <simon.duchaine@hotmail.com>
2025-08-27 07:43:27 -0400

    Update APropos.vue

--- src/components/APropos.vue
+++ src/components/APropos.vue
@@ -6,7 +6,7 @@
         </div>
         <h3 class="font-display text-4xl font-semibold pt-4 pb-14">Construction et Excavation Y.B inc. :<br> L'Art de Transformer Vos Espaces Résidentiels</h3>
         <p class="font-body text-gray-700 leading-7">La rénovation et la construction résidentielle ne sont pas simplement notre métier, c'est notre passion. Chez Construction et Excavation Y.B inc., nous sommes déterminés à dépasser les attentes de nos clients à chaque projet. Notre engagement envers la qualité se reflète dans chaque détail de notre travail, des fondations solides aux finitions impeccables. Nous mesurons notre succès par la satisfaction de nos clients et par le sourire sur leur visage lorsque leur vision devient réalité. Laissez-nous vous montrer comment nous pouvons transformer votre domicile en un espace de vie exceptionnel.</p>
-        <a class="inline-block w-full md:w-auto mb-4 mt-20 md:mr-6 py-5 px-8 text-sm font-body uppercase border-2 border-transparent bg-gray-800 hover:bg-gray-500 hover:text-gray-100 text-white transition duration-200" href="mailto:Ybureauconstruction@hotmail.com">Contactez-nous</a>
+        <a class="inline-block w-full md:w-auto mb-4 mt-20 md:mr-6 py-5 px-8 text-sm font-body uppercase border-2 border-transparent bg-gray-800 hover:bg-gray-500 hover:text-gray-100 text-white transition duration-200" href="mailto:info@constructionyb.com">Contactez-nous</a>
     </div>
 </template>
 
